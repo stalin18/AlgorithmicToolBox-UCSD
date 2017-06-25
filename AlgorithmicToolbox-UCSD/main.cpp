9150603a5bcc5c46fa1c8b6b3fb4026cb63b75cd
Kunal Swami <kunalswami@live.in>
2017-06-26 00:38:13 +0530

    Initial Commit

---
+++ AlgorithmicToolbox-UCSD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmicToolbox-UCSD
+//
+//  Created by Kunal Swami on 26/06/17.
+//  Copyright © 2017 Infinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
